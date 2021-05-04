8393c1894b6e7704c8142ca8357e0f77c51f280c
Cristian Aragón <cristian2000aragon@gmail.com>
2021-05-03 22:24:33 -0500

    comentarios, sentencias, variables, constantes

---
+++ script.js
@@ -0,0 +1,48 @@
+//hola mundo 
+alert('hola mundo');
+ 
+
+// comentarios
+/*
+   otro tipo de comentario
+
+
+sentencias 
+
+alert('hola'); 
+alert('mundo');
+
+
+inserción automática de punto y coma
+
+alert('hola') 
+alert('mundo')
+
+
+alert(3+2+1);
+
+variables 
+
+let mensaje;
+mensaje = "hola"
+alert(mensaje);
+
+mensaje = "hola2"
+alert(mensaje);
+
+let a = 5;
+let b = 2;
+let suma = a + b;
+console.log("el resultado de la suma es:" + suma);
+*/
+
+// constante
+
+const PI = 3.141624
+//const COLOR_ROJO = "#FFFFF"
+pi_dos = PI;
+pi_dos = 7;
+console.log(PI);
+console.log(pi_dos);
+
+
