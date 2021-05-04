6f3fa522fb1b85633faa622f507ae05707a19e5e
Cristian Aragón <cristian2000aragon@gmail.com>
2021-05-04 00:47:45 -0500

    Variables

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //hola mundo 
-alert('hola mundo');
+// alert('hola mundo');
  
 
 // comentarios
@@ -34,7 +34,7 @@ let a = 5;
 let b = 2;
 let suma = a + b;
 console.log("el resultado de la suma es:" + suma);
-*/
+
 
 // constante
 
@@ -44,5 +44,72 @@ pi_dos = PI;
 pi_dos = 7;
 console.log(PI);
 console.log(pi_dos);
+
+*/
+
+// tipos de datos 
+
+let mensaje_3 = "hola";
+mensaje_3 = 1234;
+
+
+//números
+
+let n = 123;
+n = 12.456;
+
+// valores numéricos especiales infinity, -infinity, Nan
+
+console.log(1/0);
+console.log(Infinity);
+
+//Nan error de calculo, resultado de una operación matemática incorrecta o indefinida 
+
+console.log("hola mundo " * 2)
+
+
+let numero_grande = 123456789897654582697125464565255n;
+console.log(numero_grande);
+
+// tipo string
+
+let texto = "hola";
+let texto_dos = 'hola';
+let texto_tres = ``; // backticks funcionalidad extendida interpolación  
+ 
+let nombre = "cristian";
+console.log(`${texto} ${nombre}`)
+
+// Booleano 
+
+let verdadero = true;
+let falso = false;
+
+let mayor = 4 > 1 ; // true 
+
+// tipo de dato null, es un valor especial que no pertenece a ningún tipo de dato 
+
+let edad = null;
+console.log(edad);
+
+let edad_dos;
+console.log(edad_dos);
+
+
+// operador tipeof 
+
+console.log(typeof undefined);
+console.log(typeof null);
+console.log(typeof 0);
+console.log(typeof true);
+console.log(typeof 10n);
+console.log(typeof "hola");
+
+console.log(typeof(alert));
+console.log(typeof(null));
+
+
+
+
 
 
